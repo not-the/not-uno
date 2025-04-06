dad3ee2c5cb38edcc5b5ca994da07fe614c8e612
not-the <87151784+not-the@users.noreply.github.com>
2025-04-06 18:56:12 -0400

    Sort cards by type after color

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -327,7 +327,11 @@ export default function Game({ game, setGame, startGame }) {
                 // Cards
                 const cardsJSX = player.cards
                     // .map((entry, index) => ({...entry, index})) // Save original index within cardData object
-                    .sort((a, b) => a.color > b.color ? 1 : -1) // Sort cards by color first, type second
+                    .sort((a, b) => { // Sort cards by color first, type second
+                        const cc = a.color.localeCompare(b.color);
+                        if(cc !== 0) return cc; // Color
+                        return a.type.localeCompare(b.type); // Type
+                    })
                     .map((cardData) => { // Create JSX
                         // const cardIndex = cardData.index;
                         return <Card

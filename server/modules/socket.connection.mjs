613cd0c626dbfa0977bbbbfdf97a938200a297b7
not-the <87151784+not-the@users.noreply.github.com>
2025-05-15 18:25:11 -0400

    Fix profile changes making debug panel btn vanish

--- server/modules/socket.connection.mjs
+++ server/modules/socket.connection.mjs
@@ -124,7 +124,7 @@ const socketConnection = function(socket) {
     
     // Set user profile
     socket.on("setUser", data => setUser(data));
-    function setUser(newUser, bypassRatelimit=false) {
+    function setUser(newUser) {
         // Type check
         if(typeof newUser !== 'object') return;
         if(newUser.name === '' || typeof newUser.name !== 'string') return;
@@ -150,25 +150,30 @@ const socketConnection = function(socket) {
             15000 : // 15 seconds (if user has updated themselves 100+ times)
             250; // 0.25 seconds
         if(
-            !bypassRatelimit &&
             socket?.name_changes >= 5 &&
             Date.now() <= (socket?.name_last_changed??0) + ratelimit
         ) return socket.emit("toast", {
             title: "Wait before trying again"
         })
 
-        // Update user
+        // Update socket
         Object.assign(socket, {
             name:               newUser?.name   ?? socket?.name   ?? "Player",
             avatar:             newUser?.avatar ?? socket?.avatar ?? arrRandom(data.avatars),
             socketID:           socket.id,
             name_changes:       (socket?.name_changes??0) + 1,
-            name_last_changed:  bypassRatelimit ? 0 : Date.now() // Timestamp
+            name_last_changed:  Date.now() // Timestamp
         })
-        if(!bypassRatelimit) socket.emit("myProfile", {
+
+        // Emit new profile
+        const profile = {
             name:   socket.name,
-            avatar: socket.avatar
-        });
+            avatar: socket.avatar,
+        }
+        if(socket.elevated) profile.elevated = true;
+        socket.emit("myProfile", profile);
+
+        // Update
         getGameByUser()?.updateClients();
     }
 
